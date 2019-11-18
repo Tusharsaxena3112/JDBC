f7f876c960e21793a78ebd95341c7ed101bc7009
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-18 10:57:33 +0530

    Initial commit

---
+++ src/jdbc/Employee.java
@@ -0,0 +1,40 @@
+package jdbc;
+
+public class Employee{
+    public String employeeName;
+    public int employee_id;
+    private double employeeSalary;
+     public Employee(String employeeName, int employee_id ,double employeeSalary){
+         this.employee_id =employee_id;
+         this.employeeName =employeeName;
+         this.employeeSalary =employeeSalary;
+     }
+
+     public void setEmployeeName(String employeeName){
+         this.employeeName =employeeName;
+     }
+
+     public void setEmployee_id(int employee_id){
+         this.employee_id =employee_id;
+     }
+
+    public void setEmployeeSalary(int employeeSalary){
+        this.employeeSalary =employeeSalary;
+    }
+
+    public String getEmployeeName(){
+         return employeeName;
+    }
+
+
+    public int getEmployee_id(){
+        return employee_id;
+    }
+
+
+    public double getEmployeeSalary(){
+        return employeeSalary;
+    }
+
+
+}
