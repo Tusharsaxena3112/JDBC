7995f77e17b6128907914fc070d6b7c82e268432
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-18 14:30:10 +0530

    adding EmployeeCRUD.java

---
+++ src/databaseHelper/EmployeeCRUD.java
@@ -0,0 +1,11 @@
+package databaseHelper;
+
+import employee.Employee;
+
+public class EmployeeCRUD {
+    private Employee employee;
+
+    public Employee createEmployee(int employee_Id, double employeeSalary, String employeeName) {
+        return new Employee();
+    }
+}
--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,4 +1,4 @@
-package jdbc;
+package employee;
 
 public class Employee{
     public String employeeName;
