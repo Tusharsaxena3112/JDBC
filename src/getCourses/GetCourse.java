fd89f16747766a92255c3c0509345f12abf23f15
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-12-07 00:42:56 +0530

    adding GetCourse.java

---
+++ src/getCourses/GetCourse.java
@@ -0,0 +1,21 @@
+package getCourses;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GetCourse {
+    public boolean getCourse(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM courses");
+        while (resultSet.next()) {
+            System.out.printf("%d\t\t%s\t\t%s\t\t%d\t\t%f", resultSet.getInt("couseID"),
+                    resultSet.getString("courseName"),
+                    resultSet.getString("courseCode"),
+                    resultSet.getInt("courseDuration"),
+                    resultSet.getDouble("courseCost"));
+        }
+        return resultSet.next();
+    }
+}
